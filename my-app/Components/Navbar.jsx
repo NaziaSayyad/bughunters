67113d9ffa8d65163749cc8f2771ab65d2151143
tarun-upadhyay <tarunu88@gmail.com>
2022-12-22 01:47:43 +0530

    nav

---
+++ my-app/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+   
+  return (
+    <div>
+         <div className="left">
+            <div className="navlogo">
+                <img src="./Logo.png" alt="" />
+            </div>
+         </div>
+         <div className="right"></div>
+    </div>
+  )
+}
+
+export default Navbar
