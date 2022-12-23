41da2f24cebaaafbab3a456049fc0b4455ba6127
abhishekyadav <yadavabhishek2723@gmail.com>
2022-12-23 12:51:57 +0530

    done

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,11 +1,8 @@
-
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../Components/navbar";
 
 import Navbar from "../Components/navbar";
 import { ChakraProvider } from "@chakra-ui/react";
-
-
 
 export default function App({ Component, pageProps }) {
   return (
